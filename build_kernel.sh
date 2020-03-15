15e5d16ccc357f33adc91c2398911c868d253762
infected_ <infectedmushi@gmail.com>
2020-03-15 09:31:20 +0000

    update build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -67,3 +67,38 @@ fi
 echo "done"
 ls -al boot.img
 echo ""
+
+echo "zipping..."
+
+VERSION="$(cat version)-$(date +%F | sed s@-@@g)"
+
+if [ -e boot.img ] ; then
+	rm infected-kernel-$VERSION.zip 2>/dev/null
+	# cp boot.img arter97-kernel-$VERSION.img
+
+	# Pack AnyKernel2
+	rm -rf kernelzip
+	mkdir -p kernelzip/dtbs
+	cp arch/arm64/boot/Image.gz-dtb kernelzip/
+	echo "
+kernel.string=infected kernel based on arter97 $(cat version) @ xda-developers
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=OnePlus5
+device.name2=OnePlus5T
+device.name3=dumpling
+device.name4=cheeseburger
+block=/dev/block/bootdevice/by-name/boot
+is_slot_device=0
+ramdisk_compression=gz
+" > kernelzip/props
+	cp -rp /mnt/Building/AnyKernel3/* kernelzip/
+	cd kernelzip/
+	7z a -mx0 arter97-kernel-$VERSION-tmp.zip *
+	zipalign -v 4 arter97-kernel-$VERSION-tmp.zip ../arter97-kernel-$VERSION.zip
+	rm arter97-kernel-$VERSION-tmp.zip
+	cd ..
+	ls -al arter97-kernel-$VERSION.zip
+fi
