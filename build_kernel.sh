5d844c2a9568ce1c113ac16249d5688e66b9dab2
infected_ <infectedmushi@gmail.com>
2020-03-15 10:47:18 +0000

    update

--- build_kernel.sh
+++ build_kernel.sh
@@ -16,7 +16,7 @@ if [ "${1}" = "skip" ] ; then
 else
 	echo "Compiling kernel"
 	cp defconfig .config
-	make "$@" || exit 1
+	ccache make -j32 || exit 1
 fi
 
 echo "Building new ramdisk"
@@ -43,20 +43,7 @@ ls -lh $RAMFS_TMP.cpio.gz
 cd $KERNELDIR
 
 echo "Making new boot image"
-mkbootimg \
-    --kernel $KERNELDIR/arch/arm64/boot/Image.gz-dtb \
-    --ramdisk $RAMFS_TMP.cpio.gz \
-    --cmdline 'androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=2048 androidboot.usbconfigfs=true androidboot.usbcontroller=a800000.dwc3 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 buildvariant=user' \
-    --base           0x00000000 \
-    --pagesize       4096 \
-    --kernel_offset  0x00008000 \
-    --ramdisk_offset 0x01000000 \
-    --second_offset  0x00f00000 \
-    --tags_offset    0x00000100 \
-    --os_version     9.0.0 \
-    --os_patch_level 2019-04 \
-    --header_version 1 \
-    -o $KERNELDIR/boot.img
+./mkbootimg --kernel $KERNELDIR/arch/arm64/boot/Image.gz-dtb --ramdisk $RAMFS_TMP.cpio.gz --cmdline 'androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=2048 androidboot.usbconfigfs=true androidboot.usbcontroller=a800000.dwc3 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 buildvariant=user' --base 0x00000000 --pagesize 4096 --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --second_offset 0x00f00000 --tags_offset 0x00000100 --os_version 9.0.11 --os_patch_level 2020-02 --header_version 1 -o $KERNELDIR/boot.img
 
 GENERATED_SIZE=$(stat -c %s boot.img)
 if [[ $GENERATED_SIZE -gt $PARTITION_SIZE ]]; then
@@ -80,8 +67,8 @@ if [ -e boot.img ] ; then
 	rm -rf kernelzip
 	mkdir -p kernelzip/dtbs
 	cp arch/arm64/boot/Image.gz-dtb kernelzip/
-	echo "
-kernel.string=infected kernel based on arter97 $(cat version) @ xda-developers
+    cd kernelzip
+	echo "kernel.string=infected kernel based on arter97 @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
@@ -92,13 +79,13 @@ device.name3=dumpling
 device.name4=cheeseburger
 block=/dev/block/bootdevice/by-name/boot
 is_slot_device=0
-ramdisk_compression=gz
-" > kernelzip/props
+ramdisk_compression=gz" > props
+    cd ..
 	cp -rp /mnt/Building/AnyKernel3/* kernelzip/
 	cd kernelzip/
-	7z a -mx0 arter97-kernel-$VERSION-tmp.zip *
-	zipalign -v 4 arter97-kernel-$VERSION-tmp.zip ../arter97-kernel-$VERSION.zip
-	rm arter97-kernel-$VERSION-tmp.zip
+	7z a -mx0 infected-kernel-$VERSION-tmp.zip *
+	zipalign -v 4 infected-kernel-$VERSION-tmp.zip ../infected-kernel-$VERSION.zip
+	rm infected-kernel-$VERSION-tmp.zip
 	cd ..
-	ls -al arter97-kernel-$VERSION.zip
+	ls -al infected-kernel-$VERSION.zip
 fi
